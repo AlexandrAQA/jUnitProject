da316c2a9576a75f08481cf69b187d1dd1893f19
Alex Panaev <panaev.as@gmail.com>
2023-06-27 22:48:51 +0400

    select 2 dropDowns and click

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -3,14 +3,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumTests {
     private final By A_FROM = By.id("afrom");
     private final By B_FROM = By.id("bfrom");
+    private final By GO_GO_GO_BTN = By.xpath(".//span[@class='gogogo']");
 
     private final String URL = "http://qaguru.lv:8089/tickets/";
     private final String DEPARTURE_AIRPORT = "RIX";
     private final String ARRIVAL_AIRPORT = "MEL";
+
 
     @Test
     public void test(){
@@ -20,7 +23,18 @@ public class SeleniumTests {
         //open homePage
         driver.get(URL);
 
+        //select departure airport
+        WebElement fromDropDown = driver.findElement(A_FROM);
+        Select departureSelect = new Select(fromDropDown);
+        departureSelect.selectByValue(DEPARTURE_AIRPORT);
 
+        //select arrival airport
+        WebElement arrivalDropDown = driver.findElement(B_FROM);
+        Select arrivalSelect = new Select(arrivalDropDown);
+        arrivalSelect.selectByValue(ARRIVAL_AIRPORT);
+
+        //find and click on GO
+        driver.findElement(GO_GO_GO_BTN).click();
 
 
     }
